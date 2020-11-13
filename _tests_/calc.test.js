77b9b7c869313448d5f783801bfdb442b1ddec14
Garrett <gman9mm@live.com>
2020-11-13 09:25:31 -0800

    Change age to 22 for a successful pass.

--- _tests_/calc.test.js
+++ _tests_/calc.test.js
@@ -2,7 +2,7 @@ import { GalacticAge } from "../src/js/calc.js";
 
 describe("GalacticAge", function () {
   it('constructor initializes planetyears and age.', () => {
-    const age = A; //my age, but feel free to use yours!
+    const age = 22; //my age, but feel free to use yours!
     const galacticAge = new GalacticAge(age);
 
     expect(galacticAge.age).toEqual(age);
