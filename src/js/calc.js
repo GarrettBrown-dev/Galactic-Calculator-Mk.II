16406de6c08117fff0d30c19d9afeb8091d0935a
Garrett <gman9mm@live.com>
2020-11-13 09:13:19 -0800

    Create Galactic Age class.

---
+++ src/js/calc.js
@@ -0,0 +1,15 @@
+export class GalacticAge {
+  constructor(age) {
+    this.age = age; //my age, although you are free to enter your own!
+    this.lifeExpectancy = 88; //just an average I picked.
+
+    this.planetYears = {
+      Mercury: .24,
+      Venus: .88,
+      Mars: 1.88,
+      Jupiter: 11.86,
+    }
+  }
+}
+
+
